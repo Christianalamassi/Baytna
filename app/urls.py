8c4ccc8ce869248fdab02e866ee8f0ca1cc85b21
Christian <christianalamassi@gmail.com>
2025-05-24 22:18:35 +0200

    Fix media root

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -11,3 +13,5 @@ urlpatterns = [
     path('impressum_english', views.impressum_english, name="impressum_english"),
     path('impressum_arabic', views.impressum_arabic, name="impressum_arabic"),
 ]
+# Serve media even when DEBUG=False (not ideal for big files, but fine on Railway)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
