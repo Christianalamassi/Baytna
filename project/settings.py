9aa2d78e95ffbabd5390c0bea4865aa8a971d2d5
Christian <christianalamassi@gmail.com>
2025-05-25 16:15:02 +0200

    Connect media with Dropbox

--- project/settings.py
+++ project/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
-    "storages",
+    'django_dropbox_storage',
 ]
 
 MIDDLEWARE = [
@@ -143,6 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+DROPBOX_ROOT_FOLDER = '/program'
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = os.environ.get("DROPBOX_OAUTH2_TOKEN")
 
