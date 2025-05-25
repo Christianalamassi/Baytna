b04664e582f49a6243db1f457350d31972fe64e1
Christian <christianalamassi@gmail.com>
2025-05-25 14:21:29 +0200

    Connect media with Dropbox and add it to requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -99,16 +99,6 @@ DATABASES = {
         # "NAME": BASE_DIR / "db.sqlite3",
     }
 
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.dropbox.DropboxStorage",
-        "OPTIONS": {},
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
