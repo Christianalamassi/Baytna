260e12a3eacd4a1021880841480f81bd8242024d
Christian <christianalamassi@gmail.com>
2025-05-25 17:29:21 +0200

    Connect media with Dropbox IV

--- app/templatetags/dropbox_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def dropbox_direct_url(url):
-    if url and "dropbox.com" in url:
-        return url.replace("?dl=0", "?raw=1")
-    return url
